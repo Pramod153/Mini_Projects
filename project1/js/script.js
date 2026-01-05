bcffb5d9d3b844e64f5e31a7850374e7acabd1dd
Pramod153 <pramodsa153155@gmail.com>
2026-01-05 21:28:33 +0530

    signup page added

--- project1/js/script.js
+++ project1/js/script.js
@@ -1,13 +1,35 @@
-function login(){
-    let user= document.getElementById("uname").value;
-    let password=document.getElementById("pass").value;
+function login() {
+    let user = document.getElementById("uname").value;
+    let password = document.getElementById("pass").value;
 
-    if(user=='admin' && password=='12345')
-    {
-        alert('user lofim success')
-    }
-    else{
-        alert('wrong password')
-    }
+    let storedUser = localStorage.getItem("username");
+    let storedPass = localStorage.getItem("password");
 
+    if (user === storedUser && password === storedPass) {
+        alert("User login success");
+        // window.location.href = "dashboard.html";
+    } else {
+        alert("Wrong username or password");
+    }
 }
+
+function registerUser() {
+    let name = document.getElementById("name").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (password !== confirmPassword) {
+        document.getElementById("msg").innerText = "Passwords do not match";
+        return false;
+    }
+
+    // Save user data
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+
+    alert("Signup Successful!\nWelcome " + name);
+    window.location.href = "../html/login.html";
+    return false;
+}
+
